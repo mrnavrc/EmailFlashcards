40a5ce3f83a2651a00fe3fc6397d2d3282a89f34
Miroslav Navratil <navratilm@outlook.com>
2022-08-26 14:05:30 +0200

    DB connection and models

--- EmailFlashcards/Data/ApplicationDbContext.cs
+++ EmailFlashcards/Data/ApplicationDbContext.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using EmailFlashcards.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EmailFlashcards.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public virtual DbSet<Flashcard> Flashcards { get; set; } = default!;
     }
 }
