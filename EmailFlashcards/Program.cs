5cefab1bfd68ddc5c49730327c1a9b630441e78e
Miroslav Navratil <navratilm@outlook.com>
2022-08-26 20:30:49 +0200

    Scaffoled LogReg

--- EmailFlashcards/Program.cs
+++ EmailFlashcards/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 
+
 // Add services to the container.
 //var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 var connectionString = builder.Configuration.GetSection("pgSettings")["pgConnection"];
