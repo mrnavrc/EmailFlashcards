0b3cee9ad718eba404632c64d01ba2f6b6d278dc
mrnavrc <mrnavratil16@gmail.com>
2022-09-21 22:38:48 +0200

    save settings

--- EmailFlashcards/Controllers/FlashcardSettingsController.cs
+++ EmailFlashcards/Controllers/FlashcardSettingsController.cs
@@ -3,6 +3,7 @@ using EmailFlashcards.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace EmailFlashcards.Controllers
@@ -23,14 +24,18 @@ namespace EmailFlashcards.Controllers
         public async Task<IActionResult> Index()
         {
             string userId = _userManager.GetUserId(User);
+
             var settings = new List<FlashcardSetting>();
             settings = await _context.FlashcardsSettings.Where(settings => settings.UserId == userId)
-                                                            .ToListAsync();
+                                                                .ToListAsync();
 
+            var FlashcardsPerDay = settings.Select(settings => settings.FlashcardsPerDay);
+            var Time = settings.Select(settings => settings.Time);
+            var FlashcardEmailAdress = settings.Select(settings => settings.FlashcardEmailAdress);
 
-            ViewData["FlashcardsPerDay"] = "8";
-            ViewData["Time"] = "12:00";
-            ViewData["FlashcardEmailAdress"] = "mr@seznam.cz";
+            ViewData["FlashcardsPerDay"] = string.Join(";", FlashcardsPerDay);
+            //ViewData["Time"] = new TimeOnly(09, 00);
+            ViewData["FlashcardEmailAdress"] = string.Join(";", FlashcardEmailAdress);
             return View();
         }
 
@@ -39,19 +44,33 @@ namespace EmailFlashcards.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         [Authorize]
-        public async Task<IActionResult> SettingsEdit(int id, [Bind("FlashcardSettingsId, UserId, FlashcardsPerDay, FlashcardEmailAdress, Time")] FlashcardSetting flashcardsettings)
+        public async Task<IActionResult> SettingsEdit(FlashcardSetting flashcardsettings)
         {
 
-            ModelState.Remove("UserId");
-            if (ModelState.IsValid)
-            { 
-                flashcardsettings.UserId = _userManager.GetUserId(User);
-                flashcardsettings.FlashcardSettingsId = 1;
+            flashcardsettings.UserId = _userManager.GetUserId(User);
+            bool RowExist = _context.FlashcardsSettings.Where(f => f.UserId == flashcardsettings.UserId)
+                                                       .Any();
+
+            if (RowExist is false)
+            {
+                _context.Add(flashcardsettings);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                int FindId = _context.FlashcardsSettings
+                                     .AsNoTracking()
+                                     .Where(f => f.UserId == flashcardsettings.UserId)
+                                     .FirstOrDefault()
+                                     .FlashcardSettingsId;
+                flashcardsettings.FlashcardSettingsId = FindId;
                 _context.Update(flashcardsettings);
                 await _context.SaveChangesAsync();
             }
-
             return RedirectToAction(nameof(Index));
         }
+
     }
 }
+
+
--- EmailFlashcards/Models/FlashcardSetting.cs
+++ EmailFlashcards/Models/FlashcardSetting.cs
@@ -8,7 +8,7 @@ namespace EmailFlashcards.Models
         public int FlashcardSettingsId { get; set; }
         public string? FlashcardEmailAdress { get; set; }
         public int FlashcardsPerDay { get; set; } = 5;
-        public TimeOnly Time { get; set; }
+        public TimeOnly Time { get; set; } = new TimeOnly(09, 00);
 
         public string? UserId { get; set; }
     }
